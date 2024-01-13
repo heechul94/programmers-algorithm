d91fc9ec20df29f2a015cdd8cf20b40193d02fb3
yan <100992153+heechul94@users.noreply.github.com>
2024-01-14 02:16:34 +0900

    [level Lv.1] Title: 가장 가까운 같은 글자, Time: 4.56 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/Lv.1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/Lv.1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,16 +1,4 @@
-// 문자열 한 글자씩 비교해야한다.
-// 한글자 앞에 같은 글자가 있으면 가장 가까운 인덱스의 차이를 구한다, 없으면 -1
 function solution(s) {
-    const answer = [-1];
-    for(let i = 0; i < s.length; i++){
-        for(let j = i-1; j > -1; j--){
-            if(s[i]===s[j]){
-                answer.push(i-j);
-                break;
-            }else if(j===0){
-                answer.push(-1);
-            }
-        }
-    }
+    var answer = [];
     return answer;
 }
