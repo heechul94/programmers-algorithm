cd7affb92650fff5ba39273437f5c220b0b47a56
yan <100992153+heechul94@users.noreply.github.com>
2024-02-09 20:52:13 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,41 +1,33 @@
-function check(string){
-    const compare = ["()","{}","[]"];
-    let changed = string;
-    let isIncluded = true;
-    // s를 순차적으로 "()", "{}", "[]"기준으로 split 하고 합쳐준다.
-    // s가 ""가 된다면 옳은 문자열 ""가 아니라면 옳지 않은 문자열이다.
-    while(isIncluded){
-        if(changed.includes("()") || changed.includes("{}") || changed.includes("[]")){
-            for(const c of compare){
-                changed = changed.split(c).join("");
-            };
-        }else{
-            isIncluded = false;
-        };
-    };
-    if(changed===""){
-        return true;
-    }else{
-        return false;
-    };
-}
-
-function convert(string){
-    let temp = "";
-    temp = string[0];
-    string = string.slice(1);
-    string = string + temp;
-    return string;
-}
 
 function solution(s) {
-    let result = 0;
-    let str = s
-    for(let i = 0; i < str.length; i++){
-        if(check(str)){
-            result++;
+    let answer = 0;
+    const str = s.split('');
+    
+    for(let j = 0; j < str.length; j++) {
+        const stack = [];
+        for(let i = 0; i < str.length; i++) {
+            const letter = str[i];
+            const len = stack.length - 1;
+            if(letter === '[' || letter === '{' || letter === '(') {
+                stack.push(letter);
+            } else {
+                if (letter === ']' && stack[len] === '[') {
+                    stack.pop();
+                } else if(letter === '}' && stack[len] === '{') {
+                    stack.pop();
+                } else if(letter === ')' && stack[len] === '(') {
+                    stack.pop();
+                } else {
+                    stack.push(letter);
+                }
+            }
         }
-        str = convert(str);
-    };
-   return result;
+        
+        stack.length === 0 && answer++;
+        str.push(str.shift());
+    }
+    
+    
+    
+    return answer;
 }
