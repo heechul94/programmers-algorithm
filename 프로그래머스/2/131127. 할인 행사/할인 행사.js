82f5ec7ff3e9e84af86bc073f099dd9db2d233d1
yan <100992153+heechul94@users.noreply.github.com>
2024-02-17 23:15:35 +0900

    [level 2] Title: 할인 행사, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,15 +1,11 @@
-// 일정금액 지불 --> 회원(10일간) <-- 매일 한가지 제품 할인
-// 할인 제품은 하루에 하나씩만 구매 가능
-
-
 function solution(want, number, discount) {
     let result = 0;
     const items = {};
     want.forEach((item,idx) => items[item]=number[idx]);
     for(let i = 0; i <= discount.length-10; i++){
         const tempItems = {...items};
-        const temp = discount.slice(i,10+i);
-        temp.forEach(item => {
+        const discounts = discount.slice(i,10+i);
+        discounts.forEach(item => {
             if(tempItems[item]){
                 tempItems[item]--;
             };
